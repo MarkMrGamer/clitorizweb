55cff8678dc1c8780f313029c8d8de7e1b2a994d
eqman93 <87844084+eqman93@users.noreply.github.com>
2021-08-08 19:51:10 -0400

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php require("lib/require/session.php"); ?>
+<html>
+<head>
+<title>clitorizweb</title>
+<link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+<center>
+<?php require("lib/require/header/header.php"); ?>
+<table class="hmcontainer" width="440">
+<tbody>
+<tr>
+<td class="hmcontainer2"><font size="+1"><b>404 Not Found</b></font><br><br><b>The page you requested does not exist.</b></td>
+</tr>
+</tbody>
+</table>
+<?php require("lib/require/footer/footer.php"); ?>
+</center>
+</body>
+</html>
