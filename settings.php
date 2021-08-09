25c99e4a72b838652b75742abbd579d7deec5220
goom <30990201+MarkMrGamer@users.noreply.github.com>
2021-08-08 17:19:42 -0700

    removed one <br>

--- settings.php
+++ settings.php
@@ -23,7 +23,7 @@
 <label style="vertical-align: top;">bio:</label> <textarea name="bio" class="UpdateText" style="width:300px;height:100px;"><?php echo $user["bio"]; ?></textarea><br><br>
  
 <label>profile picture:</label> <input type="file" name="fileupload" id="fileupload"><br><br>
-<label>audio profile:</label> <input type="file" name="fileupload2" id="fileupload2"><br><br>
+<label>audio profile:</label> <input type="file" name="fileupload2" id="fileupload2"><br>
 <br><br><input class="updateSubmit" type="submit" name="update" value="Update"></form><label>this will update audio,pfp,css and bio</label>
 <br>
 <?php require("lib/settings/counter.php"); ?>
