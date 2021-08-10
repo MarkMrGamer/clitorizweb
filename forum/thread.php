b569135616d0dd64e109b3c4aa20a96b6a9eade1
thecssdoom <88057782+thecssdoom@users.noreply.github.com>
2021-08-09 22:21:54 -0300

    fixed some stuff
    stars and rank were in wrong place

--- forum/thread.php
+++ forum/thread.php
@@ -26,6 +26,8 @@
 <font size="1" class="Profile"><a href="/profile.php?user=<?php echo $author_details["username"]; ?>"><b><?php echo $author_details["username"]; ?></b></a></font><br>
 <img src="/images/pfps/<?php echo $author_details["pfp"]; ?>.gif" class="pfp">
 <font size="1" class="Profile"><img src="<?php $badge = $author_details['badge']; require("../lib/badge.php"); ?>"></font>
+<img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png">
+<br><b>Member</b>
 <br><font size="1" class="Profile"><b>Forum Posts</b>: <?php echo $post_counter4; ?></font>
 </td></tr></tbody></table></td>
 <td class="hmcontainer2" width="300" height="30" valign="top"><table><tbody><tr>
@@ -48,11 +50,11 @@ require("lib/forum_post_counter1.php");
 <font size="1" class="Profile"><a href="/profile.php?user=<?php echo $author_details2["username"]; ?>"><b><?php echo $author_details2["username"]; ?></b></a></font><br>
 <img src="/images/pfps/<?php echo $author_details2["pfp"]; ?>.gif" class="pfp">
 <font size="1" class="Profile"><img src="<?php $badge = $author_details2['badge']; require("../lib/badge.php"); ?>"></font>
+<img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png">
+<br><b>Member</b>
 <br><font size="1" class="Profile"><b>Forum Posts</b>: <?php echo $post_counter3; ?></font>
 </td></tr></tbody></table></td>
 <td class="hmcontainer2" width="300" height="30" valign="top"><table><tbody><tr>
-<img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png"><img src="../images/star.png">
-<br><b>Member</b>
 <td><font class="Profile" size="1"><b>Posted</b>: <?php $date = strtotime($replies_details["post_date"]); echo date('d/m/Y h:i A', $date); ?></font><b><br></b><font class="Profile" size="1"><?php echo $replies_details["post_text"]; ?></font><br><br><?php if (isset($username)) { if ($username == $author_details2["username"]) { ?><font size="1"><a href="thread.php?delete_reply=<?php echo $replies_details["post_id"]; ?>">Delete</a></font><?php } } ?></td>
 </tr>
 </tbody></table></td></tr>
