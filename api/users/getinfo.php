ea944f63411842c774edb1fdad6d573af4fd8151
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-10-09 08:41:39 +0300

    Create getinfo.php

---
+++ api/users/getinfo.php
@@ -0,0 +1,19 @@
+<?php 
+require("../../lib/require/session.php"); 
+require("../../lib/config/database.php"); 
+require("../../lib/config/functions.php"); 
+require("../../lib/profile.php"); 
+require("../../forum/lib/forum_post_counter3.php");
+
+$count = 0;
+
+header("Content-Type: application/json");
+
+if (empty($_GET["user"])) { 
+  echo json_encode(array('Error' => "No user provided"));
+  die();
+}
+
+echo json_encode(array('Username' => $details["username"], 'Bio' => $details["bio"],  'Badge' => $details["badge"],'CustomStars' => $details['custom_stars'], 'CustomRank' => $details['custom_rank'], 'CustomBadge' => $details["custom_badge"], 'Status' => $details["status"], 'Picture' => $details['pfp'].'.gif'));
+
+?>
