7ee361905c2e3d5a586189a9cb1af5b49dd9625e
goom <30990201+MarkMrGamer@users.noreply.github.com>
2021-10-08 22:24:08 -0700

    ../ to the other requires and changes to fix the PFP issue
    even though the lib pfp was being a stupid function i decided to just make a variable with the details of the pfp that the user was using and with the extension ".gif" and also removed require("../lib/pfp.php")

--- api/users/list.php
+++ api/users/list.php
@@ -1,8 +1,8 @@
 <?php 
-require("../lib/require/session.php"); 
-require("../lib/config/database.php"); 
-require("../lib/config/functions.php"); 
-require("../lib/profile.php"); 
+require("../../lib/require/session.php"); 
+require("../../lib/config/database.php"); 
+require("../../lib/config/functions.php"); 
+require("../../lib/users.php"); 
 
 $jsonarr = array();
 $count = 0;
@@ -11,12 +11,13 @@ header("Content-Type: application/json");
 
 while($row = $users->fetch_assoc()) { 
  $cbadge = "";
+ $pfp = $row['pfp'] . ".gif";
  if (!empty($row['badge'])) {
    $cbadge = $row['badge'];
  } else { 
    $cbadge = $row['custom_badge'];
  }
- $userarr = array('Username' => $row['username'], 'Badge' => $cbadge, 'Status' => $row['status'], 'Picture' => require("../lib/pfp.php"));
+ $userarr = array('Username' => $row['username'], 'Badge' => $cbadge, 'Status' => $row['status'], 'Picture' => $pfp);
  $jsonarr[$count] = array('Count' => $count + 1, 'Data' => $userarr);
  $count = $count + 1;
 }
