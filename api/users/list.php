c1d0204eec367ee5f9e8341bbd4efa755ab399b5
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-10-10 13:44:22 +0300

    Update list.php

--- api/users/list.php
+++ api/users/list.php
@@ -10,11 +10,11 @@ $count = 0;
 header("Content-Type: application/json");
 
 while($row = $users->fetch_assoc()) { 
- $jsonarr[$count] = array('Username' => $row['username'], 'Badge' =>  $row['badge'], 'CustomBadge' =>  $row['custom_badge'], 'Status' => $row['status'], 'Picture' => $row['pfp'] . ".gif");
+ $jsonarr[$count] = array('username' => $row['username'], 'badge' =>  $row['badge'], 'customBadge' =>  $row['custom_badge'], 'status' => $row['status'], 'picture' => $row['pfp'] . ".gif");
  $count = $count + 1;
 }
 
 
-echo json_encode(array('Count' => $count + 1, 'Data' => $jsonarr));
+echo json_encode(array('count' => $count + 1, 'data' => $jsonarr));
 
 
