f916c7cf258f39a86ad9d840570705efa6284bf4
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-10-12 18:51:46 +0300

    Update getsubforums.php

--- api/forum/getsubforums.php
+++ api/forum/getsubforums.php
@@ -8,8 +8,6 @@ header("Content-Type: application/json");
 
 $jsonarr = array();
 
-header("Content-Type: application/json");
-
 $jsonarr[0] = array('id' => 1, 'name' => "General Discussion", 'description' => "Talk about anything else.",'threadCount' => $show_threads);
 $jsonarr[1] = array('id' => 2, 'name' => "Half-Life", 'description' => "Gordon Freeman in the flesh, or rather in the forums.",'threadCount' => $show_threads2);
 $jsonarr[2] = array('id' => 3, 'name' => "Technology", 'description' => "Talk about tech stuff.",'threadCount' => $show_threads3);
