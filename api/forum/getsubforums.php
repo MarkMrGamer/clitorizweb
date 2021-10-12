931da71d4e8a42e0ba32490f341cfc10bef75589
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-10-12 18:49:26 +0300

    Create getsubforums.php

---
+++ api/forum/getsubforums.php
@@ -0,0 +1,19 @@
+<?php 
+require("../../lib/config/database.php"); 
+require("../../lib/config/functions.php"); 
+require("../../lib/profile.php"); 
+require("../../forum/lib/forum.php");
+
+header("Content-Type: application/json");
+
+$jsonarr = array();
+
+header("Content-Type: application/json");
+
+$jsonarr[0] = array('id' => 1, 'name' => "General Discussion", 'description' => "Talk about anything else.",'threadCount' => $show_threads);
+$jsonarr[1] = array('id' => 2, 'name' => "Half-Life", 'description' => "Gordon Freeman in the flesh, or rather in the forums.",'threadCount' => $show_threads2);
+$jsonarr[2] = array('id' => 3, 'name' => "Technology", 'description' => "Talk about tech stuff.",'threadCount' => $show_threads3);
+
+echo json_encode($jsonarr);
+
+?>
