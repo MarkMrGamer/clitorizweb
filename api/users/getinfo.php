24896743969604662475f050de46f7db7266221b
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-10-12 07:38:59 +0300

    Update getinfo.php

--- api/users/getinfo.php
+++ api/users/getinfo.php
@@ -13,6 +13,6 @@ if (empty($_GET["user"])) {
   die();
 }
 
-echo json_encode(array('username' => $details["username"], 'bio' => $details["bio"],  'badge' => $details["badge"],'customStars' => $details['custom_stars'], 'customRank' => $details['custom_rank'], 'customBadge' => $details["custom_badge"].".gif", 'status' => $details["status"],  'audio' => $details["song"].".".$details["audio_file_type"],  'audioAutoplay' => $details["audio_autoplay"],  'videoAccess' => $details['video_access'], 'video' => $details["video"].".mp4", 'css' => base64_encode($details['css']), 'picture' => $details['pfp'].'.gif'));
+echo json_encode(array('username' => $details["username"], 'bio' => $details["bio"],  'badge' => $details["badge"],'customStars' => $details['custom_stars'], 'customRank' => $details['custom_rank'], 'customBadge' => $details["custom_badge"], 'status' => $details["status"],  'audio' => $details["song"].".".$details["audio_file_type"],  'audioAutoplay' => $details["audio_autoplay"],  'videoAccess' => $details['video_access'], 'video' => $details["video"].".mp4", 'css' => base64_encode($details['css']), 'picture' => $details['pfp'].'.gif'));
 
 ?>
