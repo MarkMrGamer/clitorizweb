1a1303a6ec91ba468cc8f5c47539d2d3ae4bd46c
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-11-14 19:24:27 +0300

    Update getinfo.php

--- api/users/getinfo.php
+++ api/users/getinfo.php
@@ -6,6 +6,8 @@ require ("../../forum/lib/forum_post_counter3.php");
 
 $count = 0;
 $jsonarr = array();
+$frarr = array();
+$frarr2 = array();
 
 header("Content-Type: application/json");
 
@@ -17,10 +19,37 @@ if (empty($_GET["user"]))
     die();
 }
 
-$custom_badge = $details["custom_badge"];
-$get_custom_badge = NULL;
-GetCustomBadge($custom_badge, $conn);
-$badge_detail = $get_custom_badge->fetch_assoc();
+function GetBadge(string $custom_badge, string $badge)
+{
+    $return;
+case $custom_badge != NULL:
+    $get_custom_badge = NULL;
+    GetCustomBadge($custom_badge, $conn);
+    $badge_detail = $get_custom_badge->fetch_assoc();
+    $return = "/images/custom_badges/" . $badge_detail["badge_pic"] . ".gif";
+    break;
+case $badge == "user" and $custom_badge == NULL:
+    $return = "/images/badges/user.png";
+    break;
+case $badge == "verified" and $custom_badge == NULL:
+    $return = "/images/badges/verified.png";
+    break;
+case $badge == "moderator" and $custom_badge == NULL:
+    $return = "/images/badges/moderator.png";
+    break;
+case $badge == "administrator" and $custom_badge == NULL:
+    $return = "/images/badges/administrator.png";
+    break;
+case $badge == "pfp_creator" and $custom_badge == NULL:
+    $return = "/images/badges/pfp_creator.png";
+    break;
+case $badge == "portal2" and $custom_badge == NULL:
+    $return = "/images/badges/portal2.png";
+    break;
+case $badge == "owner" and $custom_badge == NULL:
+    break;
+    return $return;
+}
 
 while ($row1 = $comments->fetch_assoc())
 {
@@ -35,13 +64,51 @@ while ($row1 = $comments->fetch_assoc())
     );
     $count = $count + 1;
 }
+
+
+
+if ($friend->num_rows > 0 or $friend2->num_rows > 0)
+{
+    if ($friend->num_rows > 0)
+    {
+        $count = 0;
+        while ($friend_details = $friend->fetch_assoc())
+        {
+            $count++;
+            $friend_details1 = $friend_details['buddy2'];
+            $friend_details2 = NULL;
+            $get_friend = NULL;
+            GetUserFriend($friend_details1, $conn);
+            $frarr[$count] = array(
+                'name' => $friend_details2["username"],
+                'picture' => $friend_details2["pfp"]
+            );
+        }
+    }
+    if ($friend2->num_rows > 0)
+    {
+        $count = 0;
+        while ($friend_details_2 = $friend2->fetch_assoc())
+        {
+            $counter2++;
+            $friend_details3 = $friend_details_2['buddy1'];
+            $friend_details4 = NULL;
+            $get_friend2 = NULL;
+            GetUserFriend2($friend_details1, $conn);
+            $frarr2[$count] = array(
+                'name' => $friend_details4["username"],
+                'picture' => $friend_details4["pfp"]
+            );
+        }
+    }
+}
+
 echo json_encode(array(
     'username' => $details["username"],
     'bio' => $details["bio"],
-    'badge' => $details["badge"],
+    'badgeImage' => GetBadge($details["custom_badge"], $details["badge"]) ,
     'customStars' => $details['custom_stars'],
     'customRank' => $details['custom_rank'],
-    'customBadge' => $badge_detail["badge_pic"] . ".gif",
     'status' => $details["status"],
     'audio' => $details["song"] . "." . $details["audio_file_type"],
     'audioAutoplay' => $details["audio_autoplay"],
@@ -49,7 +116,9 @@ echo json_encode(array(
     'video' => $details["video"] . ".mp4",
     'css' => base64_encode($details['css']) ,
     'picture' => $details['pfp'] . '.gif',
-    'comments' => $jsonarr
+    'comments' => $jsonarr,
+    'friend' => $frarr,
+    'friend2' => $frarr2
 ));
 
 ?>
